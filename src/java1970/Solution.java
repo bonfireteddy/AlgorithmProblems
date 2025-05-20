649536e23271528de01d0ad80afbe943bc77ac98
bonfireteddy <bonfireteddy@gmail.com>
2025-05-20 23:31:14 +0900

    Sove java1970

--- src/java1970/Solution.java
+++ src/java1970/Solution.java
@@ -1,10 +1,32 @@
 package java1970;
 
-public class Solution {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Solution{
+	public static void main(String[] args) throws IOException {
+		
+		System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			
+			int N = Integer.parseInt(br.readLine());	
+			String result = "";
+			
+			int[] arr = {50000,10000,5000,1000,500,100,50,10};
+			
+			for(int i = 0; i < 8; i++) {
+				int a = N/arr[i];
+				result += String.valueOf(a)+" ";
+				N -= a * arr[i];
+			}
+			
+			System.out.printf("#%d\n%s\n", t, result);
+		}
 	}
-
 }
