830128afd267d9f2178e19829147f2ce7a356cc3
bonfireteddy <bonfireteddy@gmail.com>
2025-05-20 14:14:02 +0900

    Solve java1938

--- src/java1938/Solution.java
+++ src/java1938/Solution.java
@@ -8,27 +8,27 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Student implements Comparable<Student> {
-	Double num;
+	int num;
 	Double ts; // 계산된 총합 점수
 	String tp; // 상대평가로 계산된 학점
 	
-	Student(Double num, Double mp, Double ep, Double sp){
+	Student(int num, Double mp, Double ep, Double sp){
 		this.num = num;
 		tsCalculate(mp, ep, sp);
 	}
 	
 	void tsCalculate(Double mp, Double ep, Double sp) {
 		mp = mp * 0.35;
-		ep = mp * 0.45;
-		sp = mp * 0.20;
+		ep = ep * 0.45;
+		sp = sp * 0.20;
 		
 		this.ts = mp+ep+sp;
 	}
 	
 	@Override
 	public int compareTo(Student other) {
-		// 오름차순
-		return this.ts.compareTo(other.ts);
+		// 내림차순
+		return other.ts.compareTo(this.ts);
 	}
 }
 
@@ -55,29 +55,44 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
 			
-			int result = 0;
+			String result = "";
 			
-			Student[] students = new Student[N+1];
+			Student[] students = new Student[N];
 			
 			for(int a = 1; a <= N; a++) {
 				st = new StringTokenizer(br.readLine());
 				
-				Double num = (double)a; 
+				int num = a; 
 				Double mp = Double.parseDouble(st.nextToken()); 
 				Double ep = Double.parseDouble(st.nextToken()); 
 				Double sp = Double.parseDouble(st.nextToken());
 				
 				Student student = new Student(num, mp, ep, sp);
-				students[a] = student; 
+				students[a-1] = student; 
 			}
 			
-			Arrays.sort(students, 1, N + 1); // ts 기준 오름차순 정렬
-			Arrays.sort(students, 1, N + 1, (a, b) -> a.ts.compareTo(b.ts));
+			Arrays.sort(students);
+			//Arrays.sort(students, 1, N + 1); // ts 기준 오름차순 정렬
+			//Arrays.sort(students, 1, N + 1, (a, b) -> a.ts.compareTo(b.ts));
 			// 내림차순 -> (a, b) -> b.ts.compareTo(a.ts)
 			
+			// N/10명 기준으로 상대평가 평점부여
+			int pn = N/10;
+			int gradeIndex = 0;
+			for(int a = 0; a < N; a+=pn) {
+				for(int b = a; b < a+pn; b++) {
+					students[b].tp = grade[gradeIndex];
+				}
+				gradeIndex++;
+			}
 			
+			for(int a = 0; a < N; a++) {
+				if(students[a].num == K) {
+					result = students[a].tp;
+				}
+			}
 			
-			System.out.printf("#%d %d", t, result);
+			System.out.printf("#%d %s\n", t, result);
 		}
 		
 	}
