4bdf3b38960b677b1306b2c06a7af70c8f9816d0
bonfireteddy <bonfireteddy@gmail.com>
2025-05-25 23:40:56 +0900

    Solving Solution2806 only queen trace

--- src/swea/Solution2806.java
+++ src/swea/Solution2806.java
@@ -1,11 +1,73 @@
 package swea;
 
-public class Solution2806 {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
+public class Solution2806 {
+	
+	// N*N보드에 N개의 퀸이 갈 수 있는 경로에 퀸이 겹쳐있지 않으면 된다.
+	// 1번째 퀸이 0,0 자리에 놓고 1번째 퀸이 놓인 경로를 0으로 막아둔다.
+	// 2번째 퀸이 들어갈 수 있는 자리에 놓고 2번째 퀸이 놓인 경로를 0으로 막아둔다.
+	// 3번째 퀸이 들어갈 수 있는 자리가 없는 경우 2번째 퀸을 두기 전 상태로 돌아간다.
+	// 이전에 2번째 퀸을 두었던 자리와 1번째 퀸의 경로를 제위한 다른 자리에 2번째 퀸을 둔다.
+	// 다시 3번째 퀸이 들어갈 수 있는 자리가 있는지 탐색한다.
+	// DFS, 백트래킹?
+	
+	static int[][] arr;
+	static int result;
+	static int N;
+	
 	public static void main(String[] args) throws Exception {
 		
-		System.out.println("1234");
+		System.setIn(new FileInputStream("sample_input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t < T; t++) {
+			
+			result = 0;
+			
+			N = Integer.parseInt(br.readLine());
+			
+			arr = new int[N][N];
+			
+			// 1은 갈 수 있는 경로, 0은 갈 수 없는 경로, 2는 퀸
+			for(int a = 0; a < N; a++) {
+				for(int b = 0; b < N; b++) {
+					arr[a][b] = 1;
+				}
+			}
+			
+			dfs(0, 0);
+			
+			System.out.printf("#%d %d", t, result);
+		}
+	}
+	
+	static void dfs(int a, int b) {
+		
+		int da[] = {-1,-1,-1,0,1,1,1,0};
+		int db[] = {-1,0,1,1,1,0,-1,-1};
+		
+		for(int i = 0; i < 8; i++) {
+			
+			int na = a;
+			int nb = b;
+			
+			for(int j = 0; j < N; j++) {
+				na += da[i];
+				nb += db[i];
+				
+				if(na < 0 || nb < 0 || na >= N || nb >= N ) {
+					break;
+				}
+				if(arr[na][nb] == 1) {
+					arr[na][nb] = 0;
+				}
+			}
+		}
 	}
 
 }
