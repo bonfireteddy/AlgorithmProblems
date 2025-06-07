b01686ff0f8ffcf50cd02294eafa689e6a9bc9f9
bonfireteddy <bonfireteddy@gmail.com>
2025-06-07 12:55:03 +0900

    Solve Solution1860 진기의 최고급 붕어빵

--- src/swea/Solution1860.java
+++ src/swea/Solution1860.java
@@ -11,10 +11,12 @@ public class Solution1860 {
 	// 0초 이후에 손님들이 언제 도착하는지 주어지면, 모든 손님들에게 
 	// 기다리는 시간 없이 붕어빵을 제공할 수 있는지 판별.
 	// job scheduling 인듯?
+	// 현재 몇초가 지났는지 저장해나가면서 붕어빵의 개수도 늘려간다.
+	// arr[] -> 오름차순으로 정렬시켜야함.
 	
 	public static void main(String[] args) throws Exception {
 
-		System.setIn(new FileInputStream("res/input.txt"));
+		//System.setIn(new FileInputStream("res/input.txt"));
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -29,15 +31,37 @@ public class Solution1860 {
 			int K = Integer.parseInt(st.nextToken());
 			
 			st = new StringTokenizer(br.readLine());
-			int[] arr = new int[N];
+			int[] arrival = new int[N]; // 손님들이 도착하는 시간 배열
 			for(int a = 0; a < N; a++) {
-				arr[a] = Integer.parseInt(st.nextToken());
+				arrival[a] = Integer.parseInt(st.nextToken());
 			}
 			
-			
-			
-			System.out.printf("#%d %d\n", test_case, result);
+			Arrays.sort(arrival); // 배열 오름차순 정렬
+			result = "Possible";
+            
+            for (int i = 0; i < N; i++) {
+                int t = arrival[i];
+                int breadMade = (t / M) * K; // t초까지 만든 붕어빵 수
+                int customers = i + 1;       // t초까지 도착한 손님 수
+                if (breadMade < customers) {
+                    result = "Impossible";
+                    break;
+                }
+            }
+            
+			System.out.printf("#%d %s\n", test_case, result);
 		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
