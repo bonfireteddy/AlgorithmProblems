bf311b87b5050fa8699613d1690b0d14af6be453
bonfireteddy <bonfireteddy@gmail.com>
2025-06-14 13:19:27 +0900

    refactor: BOJ 11656 substring optimization

--- src/baekjoon/Main11656.java
+++ src/baekjoon/Main11656.java
@@ -4,33 +4,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main11656{
-	
-	// 전제문자열부터 앞에서부터 하나씩 뺀 문자열들을 String배열이든 리스트든 저장하고
-	// 이 배열이나 리스트 기준으로 사전순 정렬을 적용시키면 되는데...
-	// 이게 오름차순 정렬이랑 똑같은 건가?
-	
 	public static void main(String args[]) throws Exception {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
-		
-		List<String> strList = new ArrayList<>();
-		String[] strArr = new String[line.length()];
-		
-		char[] charArr = line.toCharArray();
-		for(int a = 0; a < line.length(); a++) {
-			String str = "";
-			for(int b = a; b < line.length(); b++) {
-				str += charArr[b];
-			}
-			strArr[a] = str;
+
+		int len = line.length();
+		String[] suffixes = new String[len];
+
+		// 접미사 만들기
+		for (int i = 0; i < len; i++) {
+		    suffixes[i] = line.substring(i);
 		}
-		
-		Arrays.sort(strArr);
-		Collections.sort(strList);
-		
-		for(int a = 0; a < line.length(); a++) {
-			System.out.println(strArr[a]);
+
+		// 사전순 정렬
+		Arrays.sort(suffixes);
+
+		// 출력
+		for (String suffix : suffixes) {
+		    System.out.println(suffix);
 		}
 	}
 }
