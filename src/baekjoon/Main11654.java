e3f61988b4c3d539063dad334d534dd170c1fe07
bonfireteddy <bonfireteddy@gmail.com>
2025-06-16 10:04:01 +0900

    feat: BOJ 11654 아스키 코드

---
+++ src/baekjoon/Main11654.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class Main11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char a = sc.nextLine().charAt(0) ;
+		
+		System.out.println((int)a);
+	}
+
+}
