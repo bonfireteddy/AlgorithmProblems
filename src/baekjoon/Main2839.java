4ea6c527b17751cfa9c00de0b1e7d4bc4324d637
bonfireteddy <bonfireteddy@gmail.com>
2025-06-21 11:34:47 +0900

    feat: BOJ 2839 설탕배달 Solve

--- src/baekjoon/Main2839.java
+++ src/baekjoon/Main2839.java
@@ -1,48 +1,40 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main2839 {
-
-	public static void main(String[] args) {
+	
+	// 3과 5로 N을 만들어야할때 최소개수를 구하는 것이다.
+	// 정확하게 N을 만들 수 없을 경우는 -1을 출력
+	// N을 5로 먼저 나눈다음 3으로 나누고 이거 안되면
+	// 3으로만 해보면 되는거 아닌가? 이게 실버4문제?
+	// 5로 딱 맞아떨어지는 경우
+	// 5로 좀 되다가 3으로 되는 경우
+	// 5로 좀 되다가 3으로 안되는 경우
+	// 5의 최대개수로 안돼서 5최대개수 -1하고 3으로 되는 경우 안되는 경우
+	// 3으로 딱 맞아 떨어지는 경우
+	// 5로도 안되고 5하고 3으로도 안되고 3으로도 안되는 경우는 -1
+	
+	public static void main(String[] args) throws Exception{
 		
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int count = 0;
 
-		int n = sc.nextInt(); // 배달해야하는 kg 수
-		int cnt = 0;
-		
-		while(true) {
-			if(n % 5 == 0) {
-				System.out.println(n/5 + cnt);
-				break;
-			}
-			else if(n < 0) {
-				System.out.println(-1);
-				break;
-			}
-			n = n - 3;
-			cnt++;
+		while (N >= 0) {
+		    if (N % 5 == 0) {
+		        count += N / 5;
+		        System.out.println(count);
+		        return; // 정답을 찾았으니 종료
+		    }
+		    N -= 3;
+		    count++;
 		}
+
+		// 루프 끝났는데도 못 만들면 -1
+		System.out.println(-1);
 		
-//		int min = 0;
-//		int sum = 0;
-//		int answer = 0;
-//
-//		for (int a = 0; a <= Math.floor(N / 3); a++) { // a -> 3킬로그램 개수
-//			for (int b = 0; b <= Math.floor(N / 5); b++) { // b -> 5킬로그램 개수
-//				sum = a * 3 + b * 5;
-//				min = a+b;
-//				if(sum == N && min > a+b) { // 정확하게 N킬로그램이 만들어졌을때
-//					answer = min;
-//				}
-//			}
-//		}
-//		// 정수를 0으로 나누는 경우 ArithmeticException이 발생한다.
-//		
-//		if(min == 0) {
-//			System.out.println(-1);
-//		} else {
-//			System.out.println(answer);
-//		}
 	}
 }
