d8e8940af40248154377ee78cd453c0ecb6013f2
bonfireteddy <bonfireteddy@gmail.com>
2025-06-25 16:42:44 +0900

    refactor: BOJ 9934 add preorder + inorder + postorder

--- src/baekjoon/Main9934.java
+++ src/baekjoon/Main9934.java
@@ -16,26 +16,31 @@ public class Main9934 {
 	static int[] inorderArr; // 중위 순회 입력 배열
 	static List<Integer>[] resultLevels; // 각 레벨별 노드를 저장할 리스트 배열
 	
+	static StringBuilder inorderSB = new StringBuilder();
+    static StringBuilder preorderSB = new StringBuilder();
+    static StringBuilder postorderSB = new StringBuilder();
+	
 	/**
      * 중위 순회 배열을 이용하여 트리를 구축하고 노드를 레벨별로 분류하는 재귀 함수
      * @param start 현재 서브트리 중위 순회 시작 인덱스
      * @param end 현재 서브트리 중위 순회 끝 인덱스
      * @param depth 현재 노드의 깊이 (0이 루트)
      */
-	static void buildTreeInorder(int start, int end, int depth) {
+	static void traverse(int start, int end, int type, int depth) {
 		if(start > end) { // 기저 사례: 서브트리가 비어있을 경우
 			return;
 		}
 		
 		int mid = (start + end) / 2; // 현재 서브트리의 루트 노드는 항상 가운데 위치
 		
-		resultLevels[depth].add(inorderArr[mid]); // 해당 깊이의 리스트에 루트 노드 추가
+		if (type == 1) preorderSB.append(inorderArr[mid]).append(" "); // 전위
+        else if (type == 3)resultLevels[depth].add(inorderArr[mid]); // 해당 깊이의 리스트에 루트 노드 추가
 		
-		// 왼쪽 서브트리 재귀 호출
-		buildTreeInorder(start, mid-1, depth+1);
 		
-		// 오른쪽 서브트리 재귀 호출
-		buildTreeInorder(mid+1, end, depth+1);
+		traverse(start, mid-1, type, depth+1); // 왼쪽 서브트리 재귀 호출
+		 if (type == 0) inorderSB.append(inorderArr[mid]).append(" ");  // 중위
+		traverse(mid+1, end, type, depth+1); // 오른쪽 서브트리 재귀 호출
+		if (type == 2) postorderSB.append(inorderArr[mid]).append(" ");  // 후위
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -59,10 +64,22 @@ public class Main9934 {
 			inorderArr[a] =  Integer.parseInt(st.nextToken());
 		}
 		
-		// 재귀 호출로 트리를 구축하고 각 레벨에 노드 추가
-		buildTreeInorder(0, N-1, 0); // (시작 인덱스, 끝 인덱스, 현재 깊이)
+		// 중위 순회 (그대로 출력됨)
+		traverse(0, N-1, 0, 0); // (시작 인덱스, 끝 인덱스, 순회타입, 현재 깊이)
+		
+		// 전위 순회
+		traverse(0, N-1, 1, 0); // (시작 인덱스, 끝 인덱스, 순회타입, 현재 깊이)
+		
+		// 후위 순회
+		traverse(0, N-1, 2, 0); // (시작 인덱스, 끝 인덱스, 순회타입, 현재 깊이)
+		
+		// 레벨 순회
+		traverse(0, N-1, 3, 0); // (시작 인덱스, 끝 인덱스, 순회타입, 현재 깊이)
 		
 		// 결과 출력
+		System.out.println(inorderSB.toString());
+        System.out.println(preorderSB.toString());
+        System.out.println(postorderSB.toString());
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < K; i++) {
 			for(int node : resultLevels[i]) {
