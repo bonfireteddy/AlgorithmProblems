01ba04b668d95fccab64b0e98e95f644453493a6
bonfireteddy <bonfireteddy@gmail.com>
2025-07-23 11:09:21 +0900

    [feature] BOJ 1780 수들의 합

---
+++ src/baekjoon/Main1780.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Main1780 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long S = Long.parseLong(br.readLine());
+
+        long left = 1, right = S;
+        long ans = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long sum = mid * (mid + 1) / 2;
+
+            if (sum <= S) {
+                ans = mid;          // 더 큰 N이 가능할 수 있으므로 저장하고 계속
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
